45020bcfd16160357fd70206f7fb9d8c5c260934
Amin Esmkhani <amin.esmkhani75@gmail.com>
2023-01-03 23:30:37 +0330

    return index method in controller!

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -10,7 +10,9 @@ class ChirpController extends Controller
 
     public function index()
     {
-        return view('chirps.index');
+        return view('chirps.index',[
+           'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
     }
 
     /**
